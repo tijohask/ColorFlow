68b03a2f480c0acdf89f2243c721d22e81fef3bd
Tim Haskins <tijohask@gmail.com>
2019-02-04 20:50:12 -0500

    Comment changes
    Removed unneeded comments and added explanation comments.

--- colors.js
+++ colors.js
@@ -1,16 +1,26 @@
+//variable used to track the hue level on hsl
 var color = 0;
+//tracks saturation
 var s = "";
+//tracks lightness
 var l = "";
 
+//creates the interval that continuously changes and updates the hue,
+//as well as the toggle that turns the interval off and on again
 function colorFlow() {
+  //interval setup here
+  //updates the style, then updates the hue level
   var colorEffect = setInterval( function() {
     resetColors();
     color = color+1;
     if(color > 360){
       color = 0
     }
-    //console.log(color)
   }, 200);
+  //creates the toggle on the flow button
+  //clears the interval, 
+  //then sets the event listener back on the parent colorflow method
+  //both event listeners run exactly once
   document.getElementById('flow').addEventListener('click', function() {
     clearInterval(colorEffect);
     this.addEventListener('click', colorFlow, {once: true});
@@ -18,6 +28,8 @@ function colorFlow() {
 };
 // colorFlow()
 
+//changes the body style to reflect the values stored in color, s and l
+//updates the webpage to display those colors
 function resetColors() {
     s = document.getElementById("s").value;
     l = document.getElementById("l").value;
@@ -26,9 +38,12 @@ function resetColors() {
     document.querySelector('#sdisplay').innerHTML = s;
     document.querySelector('#ldisplay').innerHTML = l;
 }
-// no idea why this bit doesn't work...
-// returns a reference error, even though it works fine elsewhere
-document.getElementById('flow').addEventListener('click', function(){colorFlow()}, {once: true});  
+//
+resetColors();
 
+//adds an event listener to the toggle button
+document.getElementById('flow').addEventListener('click', function(){colorFlow()}, {once: true});
+
+//adds event listeners to the number inputs
 document.getElementById('s').addEventListener('change', resetColors);
 document.getElementById('l').addEventListener('change', resetColors);
